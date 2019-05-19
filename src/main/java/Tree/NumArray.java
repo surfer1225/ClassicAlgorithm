ab36e4a9076c63d8de89c87f61204cc37681bd95
surfer <xxxx@yahoo.com>
2019-05-19 18:44:40 +0800

    more on range sum

---
+++ src/main/java/Tree/NumArray.java
@@ -0,0 +1,49 @@
+package main.java.Tree;
+
+/*
+303. Range Sum Query - Immutable
+
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+ */
+public class NumArray {
+
+    int[] tree;
+    int l;
+
+    public NumArray(int[] nums) {
+        l = nums.length;
+        tree = new int[l*2];
+
+        for (int i=l;i<2*l;i++) tree[i]=nums[i-l];
+        for (int i=l-1;i>=0;i--) tree[i]=tree[2*i]+tree[2*i+1];
+    }
+
+    public int sumRange(int i, int j) {
+        i+=l;
+        j+=l;
+
+        int sum = 0;
+
+        while (i<=j) {
+            if ((i%2==1)) {
+                sum+=tree[i];
+                ++i;
+            }
+            if ((j%2==0)) {
+                sum+=tree[j];
+                --j;
+            }
+            i/=2;
+            j/=2;
+        }
+
+        return sum;
+    }
+}
---
+++ src/main/java/range_sum/ImmutableNumArr.java
@@ -0,0 +1,29 @@
+package main.java.range_sum;
+
+/*
+303. Range Sum Query - Immutable
+
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+ */
+
+public class ImmutableNumArr {
+    private int[] sum;
+
+    public ImmutableNumArr(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
